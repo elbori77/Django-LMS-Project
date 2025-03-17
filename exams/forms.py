af57c0b8426681c4ec35d0a67d78077a1193b04c
Carlos Principe <therealprincebori@gmail.com>
2025-03-17 16:59:39 -0400

    Fix style issues
    Fix style issues. All basics are in place.

--- exams/forms.py
+++ exams/forms.py
@@ -2,10 +2,30 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordResetForm, SetPasswordForm
 from django.contrib.auth.models import User
 
+class StyledForm(forms.ModelForm):
+    """ Apply Bootstrap styling to all forms. """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
+
 class StyledUserCreationForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}))
+    username = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter your email as username"}),
+        
+    )
+    password1 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Create a password"}),
+        label="Create a password",
+        help_text="Password must be at least 8 characters long, contain numbers & special characters."
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Re-enter password"}),
+        label="Re-enter password"
+    )
 
     class Meta:
         model = User
